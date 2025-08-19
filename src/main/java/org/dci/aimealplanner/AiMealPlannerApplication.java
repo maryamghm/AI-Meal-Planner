7fc63fbc328c94c87d3838c03113fe73a38a93fe
maryam <maryam.ghayoomi@gmail.com>
2025-08-19 10:20:09 +0200

    inits spring boot project

---
+++ src/main/java/org/dci/aimealplanner/AiMealPlannerApplication.java
@@ -0,0 +1,13 @@
+package org.dci.aimealplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiMealPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiMealPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dci/aimealplanner/AiMealPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dci.aimealplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiMealPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
