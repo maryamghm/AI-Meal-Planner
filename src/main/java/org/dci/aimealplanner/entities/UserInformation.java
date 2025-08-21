cb9121dd2fe17954f599d9ea32881a0e2150203c
maryam <maryam.ghayoomi@gmail.com>
2025-08-21 12:02:13 +0200

    changes the user information attributes

--- src/main/java/org/dci/aimealplanner/entities/UserInformation.java
+++ src/main/java/org/dci/aimealplanner/entities/UserInformation.java
@@ -5,12 +5,17 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.dci.aimealplanner.models.ActivityLevel;
+import org.dci.aimealplanner.models.Goal;
+import org.dci.aimealplanner.models.Sex;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name = "user_details")
+@Table(name = "user_infos")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -23,16 +28,32 @@ public class UserInformation {
     private String firstName;
     private String lastName;
     private Integer age;
-    private String gender;
+    private LocalDate birthDate;
+
+    @Enumerated(EnumType.STRING)
+    private Sex gender;
+
+    private Integer heightCm;
+    private BigDecimal weightKg;
+
+    @Enumerated(EnumType.STRING)
+    private ActivityLevel activityLevel;
+
+    @Enumerated(EnumType.STRING)
+    private Goal goal;
+    private Integer mealsPerDay;
+
+
+    private Integer targetKcalPerDay;
 
     @OneToOne
-    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @JoinColumn(name = "user_id", referencedColumnName = "id", unique = true)
     private User user;
 
     @ManyToMany
     @JoinTable(
             name = "user_dietary_preferences",
-            joinColumns = @JoinColumn(name = "user_details_id"),
+            joinColumns = @JoinColumn(name = "user_info_id"),
             inverseJoinColumns = @JoinColumn(name = "preference_id")
     )
     private Set<DietaryPreference> dietaryPreferences = new HashSet<>();
---
+++ src/main/java/org/dci/aimealplanner/models/ActivityLevel.java
@@ -0,0 +1,9 @@
+package org.dci.aimealplanner.models;
+
+public enum ActivityLevel {
+    SEDENTARY,
+    LIGHT,
+    MODERATE,
+    ACTIVE,
+    VERY_ACTIVE
+}
