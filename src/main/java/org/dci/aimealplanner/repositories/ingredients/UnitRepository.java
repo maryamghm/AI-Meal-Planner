5c2b9d0ed1ac3a176ca205352822e58fa5103407
Michael <michael@gmail.com>
2025-08-27 12:11:41 +0200

    small fix

--- src/main/java/org/dci/aimealplanner/repositories/ingredients/UnitRepository.java
+++ src/main/java/org/dci/aimealplanner/repositories/ingredients/UnitRepository.java
@@ -1,9 +1,9 @@
 package org.dci.aimealplanner.repositories.ingredients;
 
-import org.dci.aimealplanner.entities.ingredients.Ingredient;
+import org.dci.aimealplanner.entities.ingredients.Unit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UnitRepository extends JpaRepository<Ingredient, Long> {
+public interface UnitRepository extends JpaRepository<Unit, Long> {
 }
